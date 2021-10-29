7e8cdb78107f9e12b1d289c42eab8e365521b844
Golds <goldslee@foxmail.com>
2021-10-29 10:54:48 +0800

    use numpy to calculate

--- KNN_1.py
+++ KNN_1.py
@@ -20,34 +20,22 @@ def readMinistImages(filePath):
     return images
 
 
+def calculateDistance(imgI, imgII):
+    return np.sum(imgII - imgI)
+
+
 if __name__ == "__main__":
     trainImg = readMinistImages("./ministData/train-images.idx3-ubyte")
     testImg = readMinistImages("./ministData/t10k-images.idx3-ubyte")
     trainLbl = readMinistLabel("./ministData/train-labels.idx1-ubyte")
     testLbl = readMinistLabel("./ministData/t10k-labels.idx1-ubyte")
-    print(trainImg.shape, trainLbl.shape)
-    print(testImg.shape, testLbl.shape)
     num = 0
-    for img, lbl in tqdm(zip(testImg[:100], testLbl[:100])):
+    for img, lbl in tqdm(zip(testImg, testLbl)):
         distanceAndLbl = [float('inf'), 0, 0]
-        for trNum in range(len(trainImg)):
-            # cv2.imshow("re", cv2.resize(img, (640, 640)))
-            # distance = cv2.absdiff(img, trainImg[trNum])
-            trLbl = trainLbl[trNum]
-            distance = sum(sum([abs(i - j) for i, j in zip(img, trainImg[trNum])]))
-            distanceAndLbl = [distance, trLbl, trainImg[trNum].copy()] if distance < distanceAndLbl[0] else [distanceAndLbl[0], distanceAndLbl[1], distanceAndLbl[2]]
-            # cv2.imshow("training", cv2.resize(trainImg[trNum], (640, 640)))
-            # cv2.imshow("nearestBefore", cv2.resize(distanceAndLbl[2], (640, 640)))
-            # print(distanceAndLbl[:2])
-            # if trLbl == 7:
-            #     print(distance)
-            #     cv2.waitKey(0)
-            # cv2.waitKey(40)
-        # print(distanceAndLbl[:2], lbl)
+        for trImg, trLbl in zip(trainImg, trainLbl):
+            distance = calculateDistance(img, trImg)
+            distanceAndLbl = [distance, trLbl, trImg.copy()] if distance < distanceAndLbl[0] else [distanceAndLbl[0], distanceAndLbl[1], distanceAndLbl[2]]
         if distanceAndLbl[1] == lbl:
             num += 1
-        # cv2.imshow("near", cv2.resize(distanceAndLbl[2], (640, 640)))
-        # cv2.imshow("origin", cv2.resize(img, (640, 640)))
-        # cv2.waitKey(400)
-    print(num / 100)
+    print(num / testImg[0])
 
